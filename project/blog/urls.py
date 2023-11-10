9082bcb3a6eb31fd8ec90010708db78f6abda1ad
Ziyoviddin Mamadosimov <univerziyo@gmail.com>
2023-11-10 21:38:08 +0400

    create sitemap and RSS-feed

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path('<int:day>/<int:month>/<int:year>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
